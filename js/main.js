4628ff052524eebdd38a78255988a54baffe9b99
CandelariaValles <116829849+CandelariaValles@users.noreply.github.com>
2022-12-22 23:27:49 -0300

    calculo iva

--- js/main.js
+++ js/main.js
@@ -57,33 +57,46 @@ const productos = [{id: 1,nombre: "Diamond", kw: 24, precio: 2569},
 ]
 console.log (productos.length)
 
+const productosSinIva = productos.map((producto) => { return { nombre: producto.nombre, precio: producto.precio / 1.22 } })
+;
+console.log(productosSinIva);
+
 alert ("Los productos disponibles son los siguientes:")
 
 for (const producto of productos) {
     console.log (producto.kw)
     alert('La estufa ' + producto.nombre + ' con una capacidad de ' + producto.kw + ' kw. Su precio es de ' + producto.precio + ' euros.');
 }
+const productoConsumoBajo = productos.find((producto)=>producto.kw<25);
+console.log(productoConsumoBajo);
+const productoConsumoMedio = productos.find((producto)=>producto.kw>25&&producto.kw<29);
+console.log(productoConsumoMedio);
+const productoConsumoAlto = productos.find((producto)=>producto.kw>29);
+console.log(productoConsumoAlto);
 
-function productoAdapto () {
-    if (consumoEnergetico < 19){
-        const consumoBajo =productos.slice (0,1);
-        console.log (consumoBajo)
-        alert ("El producto que mejor se adapta a ti es la estufa " + consumoBajo.nombre + "de " + consumoBajo.kw +" kw. Su precio es de " + consumoBajo.precio + " euros.")
 
-    }
 
-    else if (consumoEnergetico <24) {
-        const consumoMedio = productos.slice (1,2)
-        console.log (consumoMedio)
+
+// function productoAdapto () {
+//     if (consumoEnergetico < 19){
+//         const consumoBajo =productos.slice (0,1);
+//         console.log (consumoBajo)
+//         alert ("El producto que mejor se adapta a ti es la estufa " + consumoBajo.nombre + "de " + consumoBajo.kw +" kw. Su precio es de " + consumoBajo.precio + " euros.")
+
+//     }
+
+//     else if (consumoEnergetico <24) {
+//         const consumoMedio = productos.slice (1,2)
+//         console.log (consumoMedio)
 
     
-    }
-    else {
-        const consumoAlto = productos.slice(2,productos.lenght)
-        console.log (consumoAlto)
+//     }
+//     else {
+//         const consumoAlto = productos.slice(2,productos.lenght)
+//         console.log (consumoAlto)
 
-    };
-}
-productoAdapto ();
+//     };
+// }
+// productoAdapto ();
 
 
