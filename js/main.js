ac9ddaaefbb9314a5c63174871cf17abd45de37a
CandelariaValles <116829849+CandelariaValles@users.noreply.github.com>
2022-12-22 22:58:57 -0300

    agrego productos

--- js/main.js
+++ js/main.js
@@ -1,42 +1,89 @@
-let nomeCliente
-let metriCuadri
-let altezzaSoffitto
-let tipoInfissi
-let indicelegno
+let nombreCliente
+let metrosCuadrados
+let alturaTecho
+let tipoCerramientos
+let indiceMadera
 let indicePvc
-let indiceAlluminio
+let indiceAluminio
 let indice
 
 
 function ingresoCliente() {
-    nomeCliente= prompt("Benvenuto al calcolatore di consumo energetico di Rissol. Per favore inserisca il suo nome e cognome per iniziare!")
-    if (nomeCliente == "") {
-        alert ("Non hai inserito nessun nome");
+    nombreCliente= prompt("Bienvenido a la calculadora de consumo energético de Rissol. Por favor ingrese nombre y apellido para iniciar!")
+    if (nombreCliente == "") {
+        alert ("No ha ingresado su nombre");
     }
     else {
 
-    alert ("Benvenuto/a " + nomeCliente);
+    alert ("Bienvenido/a " + nombreCliente);
 } 
 }
 
 ingresoCliente();
 
-function calcolareConsumo () {
-metriCuadri = parseFloat(prompt("Inserisca i metri cuadri della sua abitazione:"));
-altezzaSoffitto = parseFloat(prompt("Inserisca l'altezza aprossimativa del soffitto:"));
-tipoInfissi= prompt("Inserica 1- se possiede infissi in legno, 2- se possiede infissi in alluminio o 3- se possiede infissi in PVC:");
+function calcularConsumo () {
+metrosCuadrados = parseFloat(prompt("Ingrese cuántos metros cuadrados tiene su hogar:"));
+alturaTecho = parseFloat(prompt("Ingrese la altura aproximada del techo en metros:"));
+tipoCerramientos = prompt("Ingrese 1- si posee cerramientos en madera, 2- si sus cerramientos son en aluminio o 3- si sus cerramientos son en PVC:");
 
-const indice=0.05
+function calcularIndice () {
+    if (tipoCerramientos == "1") {
+        indice = 0.05
+    }
+    else if (tipoCerramientos == "2"){
+        indice = 0.04
+    }
+    else if (tipoCerramientos == "3") {
+        indice = 0.03
+    }
+    else {
+        alert ("Por favor ingrese una opción")
+        tipoCerramientos
+    }
+}
+calcularIndice ();
+console.log (calcularIndice)
 
-
-let consumoEnergetico = metriCuadri * altezzaSoffitto * indice
+let consumoEnergetico = metrosCuadrados * alturaTecho * indice
 
 console.log (consumoEnergetico)
-alert ("Il consumo della sua abitazione é di " + consumoEnergetico+ "kw.")
+alert ("El consumo aproximado de su hogar es de " + consumoEnergetico+ " kw.")
 }
-calcolareConsumo();
+calcularConsumo();
 
+const productos = [{id: 1,nombre: "Diamond", kw: 24, precio: 2569},
+{id:2, nombre: "Emerald", kw: 28, precio: 2769},
+{id:3, nombre: "Platinum", kw: 34, precio: 2969}
+]
+console.log (productos.length)
 
+alert ("Los productos disponibles son los siguientes:")
 
+for (const producto of productos) {
+    console.log (producto.kw)
+    alert('La estufa ' + producto.nombre + ' con una capacidad de ' + producto.kw + ' kw. Su precio es de ' + producto.precio + ' euros.');
+}
+
+function productoAdapto () {
+    if (consumoEnergetico < 19){
+        const consumoBajo =productos.slice (0,1);
+        console.log (consumoBajo)
+        alert ("El producto que mejor se adapta a ti es la estufa " + consumoBajo.nombre + "de " + consumoBajo.kw +" kw. Su precio es de " + consumoBajo.precio + " euros.")
+
+    }
+
+    else if (consumoEnergetico <24) {
+        const consumoMedio = productos.slice (1,2)
+        console.log (consumoMedio)
+
+    
+    }
+    else {
+        const consumoAlto = productos.slice(2,productos.lenght)
+        console.log (consumoAlto)
+
+    };
+}
+productoAdapto ();
 
 
